3f8b19319e7ea228e7f83d741b4b634534791a47
SohaAshraff <102482621+SohaAshraff@users.noreply.github.com>
2022-11-11 21:23:09 +0200

    Update savings_bank_account.cpp

--- savings_bank_account.cpp
+++ savings_bank_account.cpp
@@ -1,1 +1,13 @@
-#include "BankSystem.h"
+#include "BankAccount.h"
+int BankAccount::deposit(double amount) {
+	setBalance(balance += amount);
+	return this->balance;
+}
+int BankAccount::withdraw(double amount) {
+	if (balance < amount) {
+		cout << " This is more than what you can withdraw" << endl;
+	}
+	else
+		setBalance(balance -= amount);
+	return this->balance;
+}
